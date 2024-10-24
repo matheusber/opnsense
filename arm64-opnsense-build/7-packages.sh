526b293109cd53e181cb3c3bdddecdc9675a81ab
matheusber <lojas@arroway.org>
2024-10-23 22:08:55 -0300

    Updating for 24.7.6 and a small changes in text formatting on building scripts.

--- arm64-opnsense-build/7-packages.sh
+++ arm64-opnsense-build/7-packages.sh
@@ -4,10 +4,10 @@
 
 . env.sh
 
-echo -n "$0 Inicio:   " >> $LOG
+echo -n "$0 Inicio: " >> $LOG
 date  $DATE >> $LOG
 
 make -C /usr/tools/ VERSION=$TAG_SRC DEVICE=$DEVICE packages
 
-echo -n "$0 Final:    " >> $LOG
+echo -n "$0 Final:  " >> $LOG
 date  $DATE >> $LOG
--- arm64-opnsense-build/9-arm.sh
+++ arm64-opnsense-build/9-arm.sh
@@ -5,4 +5,4 @@ if [ -n $1 ]; then
  DEVICE=$1
 fi
 
-make -C /usr/tools/ VERSION=$TAG_SRC DEVICE=$DEVICE arm-8G
+make -C /usr/tools/ VERSION=$TAG_SRC DEVICE=$DEVICE arm-3G
