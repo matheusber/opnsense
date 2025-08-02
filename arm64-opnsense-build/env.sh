854b176a0bc63d75a25b4d66d47159603537fd31
matheusber <lojas@arroway.org>
2025-08-02 00:50:06 -0300

    Files for 25.7.1

--- arm64-opnsense-build/env.sh
+++ arm64-opnsense-build/env.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 TAG=ARM64
-TAG_SRC="25.7"
-TAG_CORE="25.7-local"
-TAG_PLUGINS="25.7"
-TAG_PORTS="25.7"
+TAG_SRC="25.7.1"
+TAG_CORE="25.7.1-local"
+TAG_PLUGINS="25.7.1"
+TAG_PORTS="25.7.1"
 VERSION="25.7"
 
 SRC_DIR=opnsense-confs
